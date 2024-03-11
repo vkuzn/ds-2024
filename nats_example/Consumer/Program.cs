ab8400384bfef061c99c3dc4e223b7e973db22ce
Vasily Kuznetsov <vasily.kuznetsov@travelline.ru>
2024-03-11 22:08:12 +0300

    Добавить описание задания pa3

---
+++ nats_example/Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using NATS.Client;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Consumer started");
+
+            ConnectionFactory cf = new ConnectionFactory();
+            using IConnection c = cf.CreateConnection();
+
+            var s = c.SubscribeAsync("valuator.processing.rank", "rank_calculator", (sender, args) =>
+            {
+                string m = Encoding.UTF8.GetString(args.Message.Data);
+                Console.WriteLine("Consuming: {0} from subject {1}", m, args.Message.Subject);
+
+            });
+
+            s.Start();
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+
+            s.Unsubscribe();
+
+            c.Drain();
+            c.Close();
+        }
+    }
+}
---
+++ nats_example/Producer/Program.cs
@@ -0,0 +1,49 @@
+﻿using NATS.Client;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Producer started");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Task.Factory.StartNew(() => ProduceAsync(cts.Token), cts.Token);
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+
+            cts.Cancel();
+
+            Console.WriteLine("done");
+        }
+
+        static async Task ProduceAsync(CancellationToken ct)
+        {
+            ConnectionFactory cf = new ConnectionFactory();
+
+            using (IConnection c = cf.CreateConnection())
+            {
+                ulong count = 0;
+                while (!ct.IsCancellationRequested)
+                {
+                    string m = $"#{count}";
+                    Console.WriteLine("Produced: {0}", m);
+                    byte[] data = Encoding.UTF8.GetBytes(m);
+                    c.Publish("valuator.processing.rank", data);
+                    await Task.Delay(1000);
+                    ++count;
+                }
+                c.Drain();
+
+                c.Close();
+            }
+        }
+    }
+}
